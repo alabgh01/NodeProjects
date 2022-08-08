fb1892664070c5a82aef809f0151f2d7983b6c80
alabgh01 <alabgh01@luther.edu>
2022-08-08 17:58:42 -0500

    05-pro

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,15 +1,20 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+
 require('dotenv').config();
 require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+app.use('/api/v1/', mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,34 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+
+const jwt = require('jsonwebtoken');
+const {BadRequestError} = require('../errors');
+
+const login = async (req, res) => {
+    const {username, password} = req.body;
+
+    if (!username || !password) {
+        throw new BadRequestError('Please provide username and password');
+    }
+
+    //just for demo, normally comes from db
+    const id = new Date().getDate();
+
+    //keep payload small for better user experience
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'});
+
+    res.status(200).json({msg:'user created', token});
+};
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random()*100);
+    res.status(200).json({
+        msg:`Hello ${req.user.username}`, 
+        secret:`Here is your authorized data, your lucky numebr: ${luckyNumber}`});
+
+};
+
+module.exports = {
+    login,
+    dashboard,
+};
